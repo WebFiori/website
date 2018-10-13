421c45f578dd5d14b3f87fd3cbed9eba25312e2c
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2018-10-14 02:36:28 +0300

    Added more API descriptions

--- pages/api-docs/1.0/entity/ConfigAPIs.php
+++ pages/api-docs/1.0/entity/ConfigAPIs.php
@@ -15,169 +15,106 @@ class ConfigAPIs extends APIView{
 
     public function defineClassFunctions() {
         $this->addFunctionDef(array(
-            'name'=>'',
-            'short-desc'=>'',
-            'long-desc'=>'',
-            'access-modifier'=>'public',
-            'params'=>array(
-                array(
-                    'name'=>'',
-                    'type'=>'',
-                    'description'=>'',
-                )
-            ),
-            'return-types'=>'',
-            'return-desc'=>''
+            'name'=>'&get',
+            'short-desc'=>'Returns an instance of the class '.$this->monoCL('entity', 'Config').'.',
+            'long-desc'=>'Returns an instance of the class '.$this->monoCL('entity', 'Config').'. Consecutive calls to '
+            . 'the function will result in returning the same instance every time.',
+            'access-modifier'=>'public static',
+            'return-types'=> $this->monoCL('entity', 'Config'),
+            'return-desc'=>'An instance of the class '.$this->monoCL('entity', 'Config').'.'
         ));
         $this->addFunctionDef(array(
-            'name'=>'',
-            'short-desc'=>'',
-            'long-desc'=>'',
-            'access-modifier'=>'public',
-            'params'=>array(
-                array(
-                    'name'=>'',
-                    'type'=>'',
-                    'description'=>'',
-                )
-            ),
-            'return-types'=>'',
-            'return-desc'=>''
+            'name'=>'getConfigVersion',
+            'short-desc'=>'Returns version number of the configuration file.',
+            'long-desc'=>'Returns version number of the configuration file. The only '
+            . 'possible use for this function is to validate compatability.',
+            'access-modifier'=>'public static',
+            'return-types'=>'string',
+            'return-desc'=>'A string in the formate \'x.x.x\'.'
         ));
         $this->addFunctionDef(array(
-            'name'=>'',
-            'short-desc'=>'',
-            'long-desc'=>'',
-            'access-modifier'=>'public',
-            'params'=>array(
-                array(
-                    'name'=>'',
-                    'type'=>'',
-                    'description'=>'',
-                )
-            ),
-            'return-types'=>'',
-            'return-desc'=>''
+            'name'=>'getDBHost',
+            'short-desc'=>'Returns the address of database host.',
+            'long-desc'=>'Returns the address of database host. Most of the '
+            . 'times, it will be \'localhost\'. But it can be something else if '
+            . 'the database is not hosted in thesameserver.',
+            'access-modifier'=>'public static',
+            'return-types'=>'string',
+            'return-desc'=>'The address of database host.'
         ));
         $this->addFunctionDef(array(
-            'name'=>'',
-            'short-desc'=>'',
-            'long-desc'=>'',
-            'access-modifier'=>'public',
-            'params'=>array(
-                array(
-                    'name'=>'',
-                    'type'=>'',
-                    'description'=>'',
-                )
-            ),
-            'return-types'=>'',
-            'return-desc'=>''
+            'name'=>'getDBName',
+            'short-desc'=>'Returns the name of the database.',
+            'long-desc'=>'Returns the name of the database.',
+            'access-modifier'=>'public static',
+            'return-types'=>'string',
+            'return-desc'=>'Returns the name of the database.'
         ));
         $this->addFunctionDef(array(
-            'name'=>'',
-            'short-desc'=>'',
-            'long-desc'=>'',
-            'access-modifier'=>'public',
-            'params'=>array(
-                array(
-                    'name'=>'',
-                    'type'=>'',
-                    'description'=>'',
-                )
-            ),
-            'return-types'=>'',
-            'return-desc'=>''
+            'name'=>'getDBPassword',
+            'short-desc'=>'Returns database user\'s password.',
+            'long-desc'=>'Returns database user\'s password.',
+            'access-modifier'=>'public static',
+            'return-types'=>'string',
+            'return-desc'=>'Returns database user\'s password.'
         ));
         $this->addFunctionDef(array(
-            'name'=>'',
-            'short-desc'=>'',
-            'long-desc'=>'',
-            'access-modifier'=>'public',
-            'params'=>array(
-                array(
-                    'name'=>'',
-                    'type'=>'',
-                    'description'=>'',
-                )
-            ),
-            'return-types'=>'',
-            'return-desc'=>''
+            'name'=>'getDBPort',
+            'short-desc'=>'Returns the port number that will be used to access server\'sdatabase.',
+            'long-desc'=>'Returns the port number that will be used to access server\'sdatabase.',
+            'access-modifier'=>'public static',
+            'return-types'=>'int',
+            'return-desc'=>'Returns the port number that will be used to access server\'sdatabase.'
         ));
         $this->addFunctionDef(array(
-            'name'=>'',
-            'short-desc'=>'',
-            'long-desc'=>'',
-            'access-modifier'=>'public',
-            'params'=>array(
-                array(
-                    'name'=>'',
-                    'type'=>'',
-                    'description'=>'',
-                )
-            ),
-            'return-types'=>'',
-            'return-desc'=>''
+            'name'=>'getDBUser',
+            'short-desc'=>'Returns the name of the user that will be used to access the '
+            . 'database.',
+            'long-desc'=>'Returns the name of the user that will be used to access the '
+            . 'database.',
+            'access-modifier'=>'public static',
+            'return-types'=>'string',
+            'return-desc'=>'Returns the name of the user that will be used to access the '
+            . 'database.'
         ));
         $this->addFunctionDef(array(
-            'name'=>'',
-            'short-desc'=>'',
-            'long-desc'=>'',
-            'access-modifier'=>'public',
-            'params'=>array(
-                array(
-                    'name'=>'',
-                    'type'=>'',
-                    'description'=>'',
-                )
-            ),
-            'return-types'=>'',
-            'return-desc'=>''
+            'name'=>'getReleaseDate',
+            'short-desc'=>'Returns the date at which the given framework version is released.',
+            'long-desc'=>'Returns the date at which the given framework version is released.',
+            'access-modifier'=>'public static',
+            'return-types'=>'string',
+            'return-desc'=>'A string that looks like this: \'09-25-2018 (DD-MM-YYYY)\'.'
         ));
         $this->addFunctionDef(array(
-            'name'=>'',
-            'short-desc'=>'',
-            'long-desc'=>'',
-            'access-modifier'=>'public',
-            'params'=>array(
-                array(
-                    'name'=>'',
-                    'type'=>'',
-                    'description'=>'',
-                )
-            ),
-            'return-types'=>'',
-            'return-desc'=>''
+            'name'=>'getVersion',
+            'short-desc'=>'Returns framework version number.',
+            'long-desc'=>'Returns framework version number.',
+            'access-modifier'=>'public static',
+            'return-types'=>'string',
+            'return-desc'=>'A string in the formate \'x.x.x\'.'
         ));
         $this->addFunctionDef(array(
-            'name'=>'',
-            'short-desc'=>'',
-            'long-desc'=>'',
-            'access-modifier'=>'public',
-            'params'=>array(
-                array(
-                    'name'=>'',
-                    'type'=>'',
-                    'description'=>'',
-                )
-            ),
-            'return-types'=>'',
-            'return-desc'=>''
+            'name'=>'getVersionType',
+            'short-desc'=>'Returns framework version type.',
+            'long-desc'=>'Returns framework version type. The type of '
+            . 'the version can be \'Beta\', \'Alpha\', and \'Stable\'. '
+            . 'The beta is a version which is still in development but '
+            . 'not recommended for production use. The alpha is mostly used '
+            . 'internally between framework developers. The stable is the one '
+            . 'that can be used in production.',
+            'access-modifier'=>'public static',
+            'return-types'=>'string',
+            'return-desc'=>'The string \'Beta\', \'Alpha\', or \'Stable\'.'
         ));
         $this->addFunctionDef(array(
-            'name'=>'',
-            'short-desc'=>'',
-            'long-desc'=>'',
-            'access-modifier'=>'public',
-            'params'=>array(
-                array(
-                    'name'=>'',
-                    'type'=>'',
-                    'description'=>'',
-                )
-            ),
-            'return-types'=>'',
-            'return-desc'=>''
+            'name'=>'isConfig',
+            'short-desc'=>'Checks if the system is configured or not.',
+            'long-desc'=>'Checks if the system is configured or not. The '
+            . 'function is used to validate system status for first timedeploy.',
+            'access-modifier'=>'public static',
+            'return-types'=>'boolean',
+            'return-desc'=>'If the system is configured, the function '
+            . 'will return '.$this->monoStr('TRUE').'.'
         ));
     }
 
--- pages/api-docs/1.0/entity/DBConnectionFactoryAPIs.php
+++ pages/api-docs/1.0/entity/DBConnectionFactoryAPIs.php
@@ -9,24 +9,70 @@ class DBConnFactoryAPIs extends APIView{
 
     public function defineClassFunctions() {
         $this->addFunctionDef(array(
-            'name'=>'',
-            'short-desc'=>'',
-            'long-desc'=>'',
+            'name'=>'mysqlLink',
+            'short-desc'=>'Create a link to MySQL database.',
+            'long-desc'=>'Create a link to MySQL database. The link can be '
+            . 'used to execute MySQL queries which is constructed using '
+            . 'an instance of '.$this->monoCL('entity/ph-mysql', 'MySQLQuery').'.',
             'access-modifier'=>'public',
             'params'=>array(
                 array(
-                    'name'=>'',
-                    'type'=>'',
-                    'description'=>'',
+                    'name'=>'$connectionParams',
+                    'type'=>'array',
+                    'description'=>'An associative array that contains database '
+                    . 'connection parameters. The indices are: '
+                    . '<ul><li><b>'.$this->monoStr('host').'</b>: Database host address.'
+                    . '</li><li><b>'.$this->monoStr('user').'</b>: Database username.</li>'
+                    . '<li><b>'.$this->monoStr('pass').'</b>: Database user\'s password.</li>'
+                    . '<li><b>'.$this->monoStr('db-name').'</b>: The name of the database (Schema name).</li>'
+                    . '</ul>',
                 )
             ),
-            'return-types'=>'',
-            'return-desc'=>''
+            'return-types'=> $this->monoCL('entity/ph-mysql', 'DatabaseLink').'|array',
+            'return-desc'=>'If the connection to the database was established, '
+            . 'the function will return an instance of '
+            . ''.$this->monoCL('entity/ph-mysql', 'DatabaseLink').'. '
+            . 'If something went wrong while attempting to connect, an '
+            . 'associative array is returned which contains error details. '
+            . 'The array has two indices: '
+            . '<ul>'
+            . '<li><b>'.$this->monoStr('error-code').'</b>: Error code. It can be MySQL error code.</li>'
+            . '<li><b>'.$this->monoStr('error-code').'</b>: A message that tels more information about '
+            . 'the error.</li></ul>'
         ));
     }
 
     public function defineClassAttributes() {
-
+        $this->addAttributeDef(array(
+            'name'=>'DB_CONNECTION_ERR',
+            'access-modifier'=>'const',
+            'short-desc'=>'A constant that indicates a database connection error has occur.',
+            'long-desc'=>'A constant that indicates a database connection error has occur.'
+        ));
+        $this->addAttributeDef(array(
+            'name'=>'MISSING_DB_HOST',
+            'access-modifier'=>'const',
+            'short-desc'=>'A constant that indicates the name of database host is missing.',
+            'long-desc'=>'A constant that indicates the name of database host is missing.'
+        ));
+        $this->addAttributeDef(array(
+            'name'=>'MISSING_DB_NAME',
+            'access-modifier'=>'const',
+            'short-desc'=>'A constant that indicates the name of the database is missing.',
+            'long-desc'=>'A constant that indicates the name of the database is missing.'
+        ));
+        $this->addAttributeDef(array(
+            'name'=>'MISSING_DB_PASS',
+            'access-modifier'=>'const',
+            'short-desc'=>'A constant that indicates the user password of the database is missing.',
+            'long-desc'=>'A constant that indicates the user password of the database is missing.'
+        ));
+        $this->addAttributeDef(array(
+            'name'=>'MISSING_DB_USER',
+            'access-modifier'=>'const',
+            'short-desc'=>'A constant that indicates the username that is used to access the database is missing.',
+            'long-desc'=>'A constant that indicates the username that is used to access the database is missing.'
+        ));
     }
 }
 new DBConnFactoryAPIs();
