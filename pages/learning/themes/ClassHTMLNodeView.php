f439413679b9e059c799b14ea70f19a895af805d
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-01-29 17:26:50 +0300

    Update ClassHTMLNodeView.php

--- pages/learning/themes/ClassHTMLNodeView.php
+++ pages/learning/themes/ClassHTMLNodeView.php
@@ -3,6 +3,10 @@ namespace webfiori\views\learn\themes;
 use webfiori\views\WebFioriPage;
 use webfiori\WebFiori;
 use webfiori\entity\Page;
+use phpStructs\html\PNode;
+use phpStructs\html\HTMLNode;
+Use phpStructs\html\UnorderedList;
+use phpStructs\html\ListItem;
 use WebFioriGUI;
 /**
  * Description of ClassThemeView
@@ -17,7 +21,39 @@ class ClassThemeView extends WebFioriPage{
             'canonical'=> WebFiori::getSiteConfig()->getBaseURL().'learn/topics/themes/class-HTMLNode'
         ));
         WebFioriGUI::createTitleNode(Page::title());
+        
+        $this->createParagraph('This class acts as an interface between the framework and HTML. It represents any '
+                . 'HTML element you can think of. The class <a href="'.WebFiori::getSiteConfig()->getBaseURL()
+                .'docs/docs/phpStructs/html/HTMLNode" target="_blank">HTMLNode</a> provide the following utilites to '
+                . 'create and manipulate HTML elements within the page:');
+        $this->classUtilitiesUL();
+        $this->createParagraph('The developer can create sub-classes of this class to create different types of '
+                . 'HTML elements or add extra functionality.');
         $this->displayView();
+    }
+    
+    private function classUtilitiesUL(){
+        $ul = new UnorderedList();
+        Page::insert($ul);
+        $li1 = new ListItem();
+        $li1->addTextNode('The ability to create any DOM element.');
+        $ul->addChild($li1);
+        
+        $li2 = new ListItem();
+        $li2->addTextNode('The ability to append and remove child DOM elements.');
+        $ul->addChild($li2);
+        
+        $li3 = new ListItem();
+        $li3->addTextNode('The ability to set and get the values of element attributes.');
+        $ul->addChild($li3);
+        
+        $li4 = new ListItem();
+        $li4->addTextNode('The ability to add text or comment elements');
+        $ul->addChild($li4);
+        
+        $li5 = new ListItem();
+        $li5->addTextNode('The ability to display the element as formatted HTML code in the browser.');
+        $ul->addChild($li5);
     }
 }
 new ClassThemeView();
