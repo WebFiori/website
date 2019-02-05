830cf0e602f588f3aa1698727664b8329c10e44e
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-02-05 17:29:54 +0300

    updates

--- pages/learning/themes/ClassHTMLDocView.php
+++ pages/learning/themes/ClassHTMLDocView.php
@@ -11,6 +11,7 @@ use webfiori\WebFiori;
 use phpStructs\html\HTMLDoc;
 use webfiori\entity\Page;
 use phpStructs\html\HTMLNode;
+use phpStructs\html\CodeSnippet;
 /**
  * Description of ClassHTMLDocView
  *
@@ -23,10 +24,15 @@ class ClassHTMLDocView extends ThemesLearnView{
             'description'=>'',
             'canonical'=> WebFiori::getSiteConfig()->getBaseURL().'learn/topics/themes/class-HTMLDoc'
         ));
-        
+        Page::document()->getHeadNode()->addCSS('themes/webfiori/css/code-theme.css');
         $this->createParagraph('The class <a href="'.WebFiori::getSiteConfig()->getBaseURL().'docs/phpStructs/html/HTMLDoc" '
-                . 'target="_blank">HTMLDoc</a> represent HTML 5 document. ');
-        
+                . 'target="_blank">HTMLDoc</a> represent HTML 5 document. By default, the document that will be generated '
+                . 'by the class will look like the following.');
+        $code = new CodeSnippet();
+        $code->setTitle('HTML Code');
+        $doc = new HTMLDoc();
+        $code->setCode($doc->asCode(array('use-pre'=>FALSE)));
+        Page::insert($code);
         $this->setPrevTopicLink('learn/topics/themes/class-HTMLNode', 'The class \'HTMLNode\'');
         $this->setNextTopicLink('learn/topics/themes/class-HeadNode', 'The class \'HeadNode\'');
         $this->displayView();
