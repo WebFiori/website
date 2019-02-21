8c44e6a1e478a8dc4892c45de0024b50b3a656bc
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-02-21 17:28:22 +0300

    Update Index.php

--- pages/learning/routing/Index.php
+++ pages/learning/routing/Index.php
@@ -18,6 +18,13 @@ class Index extends RoutingLearnView{
             'description'=>'Learn about the basics of routing in '
             . 'WebFiori Framework.'
         ));
+        $this->createParagraph('One of the essential parts of the framework is routing. '
+                . 'Routing in simple trems is sending user request to its correct '
+                . 'destination. The developer can use the class <a href="docs/webfiori/entity/router/Router" target="_blank">Router</a>.');
+        $this->createParagraph('After finishing the following set of topics, you will '
+                . 'be able to understand how routing sub-system works and create '
+                . 'your own custom routes.');
+        
         $this->displayView();
     }
 }
