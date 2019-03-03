ef24e91d7786d1e9ba96c0b9ead7bab24f5d7cd5
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-03-03 16:40:40 +0300

    Update MoreAboutViewsView.php

--- pages/learning/intro/MoreAboutViewsView.php
+++ pages/learning/intro/MoreAboutViewsView.php
@@ -7,6 +7,7 @@
  */
 
 namespace webfiori\views\learn\intro;
+use webfiori\entity\Page;
 
 /**
  * Description of MoreAboutViewsView
@@ -21,6 +22,20 @@ class MoreAboutViewsView extends IntroLearnView{
             . 'how to use the class \'Page\'.',
             'active-aside'=>3
         ));
+        Page::insert($this->createParagraph(''
+                . 'Views in WebFiori Framework can be more than just plain HTML. '
+                . 'They can be dynamic PHP web pages. One of the grate features that '
+                . 'the framework provides is the entity class \'<a href="docs/webfiori/entity/Page" target="_blank">Page</a>\'.'
+                . ''));
+        $sec1 = $this->createSection('The Class \'Page\'');
+        $sec1->addChild($this->createParagraph(''
+                . 'The class \'Page\' simply represents a dynamic PHP web page. '
+                . 'The developer can use this class to change many attributes of a '
+                . 'web page such as its title, description, canonical URL and more. '
+                . 'You can learn more about this class in <a href="learn/topics/themes" target="_blank">'
+                . 'Themes Lessons</a> under the section '
+                . '<a href="learn/topics/themes/class-Page" target="_blank">The Class \'Page\'</a>.'
+                . ''));
         $this->displayView();
     }
 }
