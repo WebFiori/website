acc76b4522f31ffd0383339b0489e0ad92d3c092
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-09-15 21:26:46 +0300

    Update DownloadView.php

--- pages/DownloadView.php
+++ pages/DownloadView.php
@@ -34,7 +34,7 @@ class DownloadView extends WebFioriPage{
     private function _stableDownloads() {
         $sec = $this->createSection('Stable Releases');
         $sec->addChild($this->createParagraph('The latest release of the framework is version '
-                . '1.0.2. You can click <a href="downloads/webfiori-v1.0.2-stable">here</a> in order to start the '
+                . '1.0.3. You can click <a href="downloads/webfiori-v1.0.3-stable">here</a> in order to start the '
                 . 'download process.'));
         Page::insert($sec);
     }
@@ -44,6 +44,7 @@ class DownloadView extends WebFioriPage{
                 . 'not good option for production and might have bugs.'));
         $ul = new UnorderedList();
         $ul->addListItems(array(
+            '<a href="downloads/webfiori-v1.0.2-beta-1">WebFiori v1.0.2 Stable</a>',
             '<a href="downloads/webfiori-v1.0.2-beta-1">WebFiori v1.0.2 Beta 1</a>',
             '<a href="downloads/webfiori-v1.0.0-stable">WebFiori v1.0.0 Stable</a>',
             '<a href="downloads/webfiori-v1.0.0-beta-2">WebFiori v1.0.0 Beta 2</a>',
