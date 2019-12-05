9df962b08eab035e9caaeaa4bbe956a8b29e2085
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-12-05 19:48:00 +0300

    Update IndexView.php

--- pages/video-tutorials/IndexView.php
+++ pages/video-tutorials/IndexView.php
@@ -21,12 +21,13 @@ class ContributeView extends WebFioriPage{
                 . 'most of the things that you need to get started with '
                 . 'WebFiori Framework. The creator of the videos is the '
                 . 'author of the video.'));
-        $this->_createVideoSection('Introduction','An introduction to WebFiori Framework.', 'UiIHQsZ-b2A', 'intro');
-        $this->_createVideoSection('Installing WAMP Stack','How to install WAMP Stack and setup local development environment.', 'FD_ZLMwnRD8', 'installing-wamp-stack');
-        $this->_createVideoSection('Creating PHP Project','How to create a WebFiori Framework based PHP project using Apache NetBeans IDE.', '8nCTqtUsRAg', 'creating-first-project');
+        $this->_createVideoSection('Introduction','An introduction to WebFiori Framework.', 'rxfy1f0PGHw', 'intro');
+        $this->_createVideoSection('Installing WAMP Stack','How to install WAMP Stack and setup local development environment.', '5JhFgz8Iycw', 'installing-wamp-stack');
+        $this->_createVideoSection('Creating PHP Project','How to create a WebFiori Framework based PHP project using Apache NetBeans IDE.', 'h7tCZLfSvmE', 'creating-first-project');
         $this->_createVideoSection('Routing - Part 1','Introduction to routing.', 'n7ZC-ti5XkM', 'routing-p1');
         $this->_createVideoSection('Routing - Part 2','Different types of routes and how to create them.', 'UEQhVRVG7b4', 'routing-p2');
-        $this->_createVideoSection('Views - Part 1','Learn about the basic components of building HTML pages.', 'u1qs1Mu-83c', 'views-p1');
+        $this->_createVideoSection('Views - Part 1','Learn about the basic components of building HTML pages.', 'ny1nz_zfUs4', 'views-p1');
+        $this->_createVideoSection('Views - Part 2','An introduction to the class "Page".', 'sFdRiokvThg', 'views-p2');
         $this->displayView();
     }
     private function _createVideoSection($title,$desc,$vidId,$vidPageLink){
