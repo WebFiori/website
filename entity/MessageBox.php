7aab07bf6a2107ff83dcb5f1367a9b3efcfae0f8
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-12-18 01:07:26 +0300

    Update MessageBox.php

--- entity/MessageBox.php
+++ entity/MessageBox.php
@@ -61,15 +61,15 @@ class MessageBox extends HTMLNode{
         $this->setStyle([
             'width'=>'75%',
             'border'=>'1px double white',
-            'height'=>'130px',
+            'height'=>'150px',
             'margin'=>'0px',
-            'z-index'=>'100',
+            'z-index'=>'3000',
             'position'=>'fixed',
             'background-color'=>'rgba(0,0,0,0.7)',
             'color'=>'white',
             'height'=>'auto',
-            'top'=> (self::getCount()*10).'px',
-            'left'=> (self::getCount()*10).'px'
+            'top'=> (self::getCount()*5).'px',
+            'left'=> (self::getCount()*5).'px'
         ]);
         $this->_createHeader();
         $this->_createBody();
