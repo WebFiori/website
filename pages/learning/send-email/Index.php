885411f1b7e08625948fadb530f117b1f4425337
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-12-27 16:03:55 +0300

    Update Index.php

--- pages/learning/send-email/Index.php
+++ pages/learning/send-email/Index.php
@@ -1,6 +1,7 @@
 <?php
 namespace webfiori\views\learn\mailing;
-
+use webfiori\entity\Page;
+use phpStructs\html\UnorderedList;
 /**
  * Description of Index
  *
@@ -16,7 +17,27 @@ class Index extends MailingLearnView{
             . 'has all needed tools to allow the system be able to send '
             . 'HTML emails.'
         ]);
-        $this->setNextTopicLink('learn/topics/mailing/classes', 'Required Classes');
+        Page::insert($this->createParagraph('One of the important features of any web application '
+            . 'is the ability to send email messages. WebFiori Framework '
+            . 'has all needed tools to allow the system be able to send '
+            . 'HTML emails. '
+            .'Email messages are used in many ways. '
+            . 'For example, they are used to activate user account, reset password, '
+            . 'send ads, etc...'));
+        Page::insert($this->createParagraph(''
+                . 'After finishing the following set of topics, you will be able to understand how '
+                . 'to send a very basic email message using the framework.'
+                . ''));
+        $sec = $this->createSection('Topics Covered:');
+        Page::insert($sec);
+        $topicsUl = new UnorderedList();
+        $topicsUl->addListItems([
+            '<a href="learn/mail/classes">Mailing Sub-system Classes</a>',
+            '<a href="learn/mail/send-email">How to Send an Email</a>',
+            '<a href="learn/mail/attachments">Adding Attachments to Your Email</a>'
+        ], false);
+        $sec->addChild($topicsUl);
+        $this->setNextTopicLink('learn/topics/mailing/classes', 'Mailing Sub-system Classes');
         $this->displayView();
     }
 }
