ae4d9c167e681159adb5e73363a2532ee767fb92
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-01-01 17:29:37 +0300

    Update IndexView.php

--- pages/video-tutorials/IndexView.php
+++ pages/video-tutorials/IndexView.php
@@ -21,18 +21,29 @@ class ContributeView extends WebFioriPage{
                 . 'most of the things that you need to get started with '
                 . 'WebFiori Framework. The creator of the videos is the '
                 . 'author of the video.'));
-        $this->_createVideoSection('Introduction','An introduction to WebFiori Framework.', 'rxfy1f0PGHw', 'intro');
-        $this->_createVideoSection('Installing WAMP Stack','How to install WAMP Stack and setup local development environment.', '5JhFgz8Iycw', 'installing-wamp-stack');
-        $this->_createVideoSection('Creating PHP Project','How to create a WebFiori Framework based PHP project using Apache NetBeans IDE.', 'h7tCZLfSvmE', 'creating-first-project');
-        $this->_createVideoSection('Routing - Part 1','Introduction to routing.', 'n7ZC-ti5XkM', 'routing-p1');
-        $this->_createVideoSection('Routing - Part 2','Different types of routes and how to create them.', 'UEQhVRVG7b4', 'routing-p2');
-        $this->_createVideoSection('Views - Part 1','Learn about the basic components of building HTML pages.', 'ny1nz_zfUs4', 'views-p1');
-        $this->_createVideoSection('Views - Part 2','An introduction to the class "Page".', 'sFdRiokvThg', 'views-p2');
+        $bodyRow = Page::theme()->createHTMLNode(['type'=>'row']);
+        Page::insert($bodyRow);
+        
+        $bodyRow->addChild($this->_createVideoBox('Introduction','An introduction to WebFiori Framework.', 'rxfy1f0PGHw', 'intro'));
+        $bodyRow->addChild($this->_createVideoBox('Installing WAMP Stack','How to install WAMP Stack and setup local development environment.', '5JhFgz8Iycw', 'installing-wamp-stack'));
+        $bodyRow->addChild($this->_createVideoBox('Creating PHP Project','How to create a WebFiori Framework based PHP project using Apache NetBeans IDE.', 'h7tCZLfSvmE', 'creating-first-project'));
+        $bodyRow->addChild($this->_createVideoBox('Routing - Part 1','Introduction to routing.', 'n7ZC-ti5XkM', 'routing-p1'));
+        $bodyRow->addChild($this->_createVideoBox('Routing - Part 2','Different types of routes and how to create them.', 'UEQhVRVG7b4', 'routing-p2'));
+        $bodyRow->addChild($this->_createVideoBox('Views - Part 1','Learn about the basic components of building HTML pages.', 'ny1nz_zfUs4', 'views-p1'));
+        $bodyRow->addChild($this->_createVideoBox('Views - Part 2','An introduction to the class "Page".', 'sFdRiokvThg', 'views-p2'));
         $this->displayView();
     }
-    private function _createVideoSection($title,$desc,$vidId,$vidPageLink){
+    /**
+     * 
+     * @param type $title
+     * @param type $desc
+     * @param type $vidId
+     * @param type $vidPageLink
+     * @return HTMLNode
+     */
+    private function _createVideoBox($title,$desc,$vidId,$vidPageLink){
         $link = Page::canonical().'/'.$vidPageLink;
-        $vidRow = Page::theme()->createHTMLNode(['type'=>'wf-row']);
+        $vidRow = Page::theme()->createHTMLNode(['type'=>'col','size'=>3]);
         $vidRow->setStyle([
             'border'=>'1px solid'
         ]);
@@ -42,14 +53,12 @@ class ContributeView extends WebFioriPage{
         $vidImg->setStyle([
             'width'=>'110px'
         ]);
-        $vidImg->setClassName('wf-ltr-col-3');
         $vidRow->addTextNode('<a href="'.$link.'">'.$vidImg.'</a>',false);
         $vidTitleContainer = new HTMLNode();
-        $vidTitleContainer->setClassName('wf-ltr-col-7');
         $vidTitleContainer->addTextNode('<p><a href="'.$link.'"></p>'.$title.'</a>',false);
         $vidTitleContainer->addTextNode('<p>'.$desc.'</p>',false);
         $vidRow->addChild($vidTitleContainer);
-        Page::insert($vidRow);
+        return $vidRow;
     }
 }
 new ContributeView();
