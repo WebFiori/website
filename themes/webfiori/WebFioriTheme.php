d0eacfe009e30ff7464322fec1a6a60ba2f7ce8b
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-05-07 02:13:31 +0300

    Update WebFioriTheme.php

--- themes/webfiori/WebFioriTheme.php
+++ themes/webfiori/WebFioriTheme.php
@@ -5,7 +5,7 @@ use webfiori\entity\Theme;
 use webfiori\entity\Page;
 use webfiori\logic\WebsiteController;
 use phpStructs\html\ListItem;
-use phpStructs\html\LinkNode;
+use phpStructs\html\Anchor;
 use phpStructs\html\HeadNode;
 use phpStructs\html\HTMLNode;
 use phpStructs\html\Input;
@@ -65,22 +65,22 @@ class WebFioriTheme extends Theme{
             $mainMenu = Page::document()->getChildByID('menu-items-container');
 
             $item1 = new ListItem();
-            $link1 = new LinkNode(SiteConfig::getBaseURL().'download', $translation->get('menus/main-menu/menu-item-1'));
+            $link1 = new Anchor(SiteConfig::getBaseURL().'download', $translation->get('menus/main-menu/menu-item-1'));
             $item1->addChild($link1);
             $mainMenu->addChild($item1);
 
             $item2 = new ListItem();
-            $link2 = new LinkNode(SiteConfig::getBaseURL().'docs/webfiori', $translation->get('menus/main-menu/menu-item-2'));
+            $link2 = new Anchor(SiteConfig::getBaseURL().'docs/webfiori', $translation->get('menus/main-menu/menu-item-2'));
             $item2->addChild($link2);
             $mainMenu->addChild($item2);
 
             $item3 = new ListItem();
-            $link3 = new LinkNode(SiteConfig::getBaseURL().'learn', $translation->get('menus/main-menu/menu-item-3'));
+            $link3 = new Anchor(SiteConfig::getBaseURL().'learn', $translation->get('menus/main-menu/menu-item-3'));
             $item3->addChild($link3);
             $mainMenu->addChild($item3);
             
             $item4 = new ListItem();
-            $link4 = new LinkNode(SiteConfig::getBaseURL().'contribute', $translation->get('menus/main-menu/menu-item-4'));
+            $link4 = new Anchor(SiteConfig::getBaseURL().'contribute', $translation->get('menus/main-menu/menu-item-4'));
             $item4->addChild($link4);
             $mainMenu->addChild($item4);
         });
@@ -186,7 +186,7 @@ class WebFioriTheme extends Theme{
         $img->setClassName('wf-'.Page::dir().'-col-1-np-nm');
         $img->setID('logo');
         $img->setWritingDir(Page::dir());
-        $link = new LinkNode(SiteConfig::getHomePage(), '');
+        $link = new Anchor(SiteConfig::getHomePage(), '');
         $link->addChild($img);
         $headerSec->addChild($link);
         $session = WebsiteController::get()->getSession();
