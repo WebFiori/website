0c20a1780e8e941db0eb1226f5325ef1dda5c0af
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-05-27 03:43:13 +0300

    Update ReadingInput.php

--- pages/learning/cli/ReadingInput.php
+++ pages/learning/cli/ReadingInput.php
@@ -2,6 +2,7 @@
 namespace webfiori\views\learn\cli;
 
 use webfiori\entity\Page;
+use phpStructs\html\CodeSnippet;
 
 class ReadingInput extends CLILearnView{
     public function __construct() {
@@ -25,6 +26,20 @@ class ReadingInput extends CLILearnView{
                 . 'user. The second one is a default value which will be used when the user hit '
                 . '"Enter" without typing anything. The last one is a closure which can be used '
                 . 'to validate user input.'));
+        $sec1->addChild($this->createParagraph('The following code snippit shows how to get user input using the method '
+                . '<code>CLICommand::getInput()</code>.'));
+        $code1 = new CodeSnippet();
+        $code1->setTitle('PHP');
+        $code1->setCode("\$userInput = \$this->getInput('Give me your name:');\n"
+                . "\$this->println(\"Your name is: \$userInput\");");
+        $sec1->addChild($code1);
+        $sec1->addChild($this->createParagraph('The following code snippit shows how to use default values with prompt. In this case, '
+                . 'the default value will be "Orange".'));
+        $code2 = new CodeSnippet();
+        $code2->setTitle('PHP');
+        $code2->setCode("\$userInput = \$this->getInput('Enter a fruit name:', 'Orange');\n"
+                . "\$this->println(\"Fruit name: \$userInput\");");
+        $sec1->addChild($code2);
         Page::insert($sec1);
         $sec2 = $this->createSection('Confirm');
         $sec2->addChild($this->createParagraph(''
@@ -35,12 +50,53 @@ class ReadingInput extends CLILearnView{
                 . 'specifying an answer. The method will return true if the answer was "yes" and false if the '
                 . 'answer is "no".'
                 . ''));
-        $sec2->addChild($this->createParagraph('To submit the answer, the user have to type <code>n<code> for "no" or '
-                . '<code>y</code> for "yes" in the console and hit "Enter".'));
+        $sec2->addChild($this->createParagraph('To submit the answer, the user have to type <code>n</code> for "no" or '
+                . '<code>y</code> for "yes" in the console and hit "Enter". Note that if the user submitted any other answer, the '
+                . 'method will keep asking till he submit one of the two answers.'));
+        $sec2->addChild($this->createParagraph('The following code sample shows how to use the method <code>CLICommand::confirm()</code>.'));
+        $code3 = new CodeSnippet();
+        $code3->setTitle('PHP');
+        $code3->setCode("if (\$this->confirm('Are you sure that you would like to continue?')) {\n"
+                . "    \$this->success('Will continue.');\n"
+                . "} else {\n"
+                . "    \$this->warning('Will stop here.');\n"
+                . "}\n");
+        $sec2->addChild($code3);
+        $sec2->addChild($this->createParagraph('The following image shows the output of the command when executed.'));
+        $sec2->addChild($this->createImag('assets/images/cli00.png', 'Terminal Output'));
         Page::insert($sec2);
         $sec3 = $this->createSection('Multiple Choice Question');
-        $sec3->addChild($this->createParagraph(''));
+        $sec3->addChild($this->createParagraph('Amultiple choice input is a way to make the user select one '
+                . 'of more than one choice. The method <a href="docs/webfiori/entity/cli/CLICommand#select" target="_blank">CLICommand::select()</a> '
+                . 'is used to show the question and the possible choices. The method accepts 3 parameters. The first one is '
+                . 'the text that will be shown to the user. The second one is the array that contains the '
+                . 'choices at which the user will select from. The last parameter is an optional '
+                . 'default value to use in case the user hit "Enter" without selecting any choice. Note that '
+                . 'the array must be indexed and the default value must be part of the choices.'));
+        $sec3->addChild($this->createParagraph('The user can select an answer by supplying its number as '
+                . 'an input or the full text of the choice.'));
+        $sec3->addChild($this->createParagraph('The following code sample shows how to use the method <code>CLICommand::select()</code>.'));
+        $code4 = new CodeSnippet();
+        $code4->setTitle('PHP');
+        $code4->setCode(""
+                . "\$choices = [\n"
+                . "    'PHP',\n"
+                . "    'JavaScript',\n"
+                . "    'C++','HTML',\n"
+                . "    'C#'\n"
+                . "];\n"
+                . "\$answer = \$this->select('Which of the following is not a programming language?', \$choices);\n\n"
+                . "if (\$answer == 'HTML') {\n"
+                . "    \$this->success('Correct Answer');\n"
+                . "} else {\n"
+                . "    \$this->error('Wrong answer.');\n"
+                . "}");
+        $sec3->addChild($code4);
+        $sec3->addChild($this->createParagraph('The following image shows the output of the command when executed.'));
+        $sec3->addChild($this->createImag('assets/images/cli01.png', 'Terminal Output'));
         Page::insert($sec3);
+        $this->setPrevTopicLink('learn/topics/cli/using-args', 'Using Command Line Arguments');
+        $this->setNextTopicLink('learn/topics/cli/formatting-output', 'Formatting Output');
         $this->displayView();
     }
 }
