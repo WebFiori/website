4b93d042a2af800b6234c24f1ed94c500ea8e5e8
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-05-29 19:31:33 +0300

    Update QuestionsAndAnswers.php

--- pages/learning/cli/QuestionsAndAnswers.php
+++ pages/learning/cli/QuestionsAndAnswers.php
@@ -1,14 +1,24 @@
 <?php
 
 namespace webfiori\views\learn\cli;
+use webfiori\entity\Page;
 
 class QuestionsAndAnswers extends CLILearnView {
     public function __construct() {
         parent::__construct([
             'title' =>'Questions and Answers About Command Line Interface',
-            'description' => '',
+            'description' => 'Here you will find a list of some of the common '
+                . 'questions about command line applications support and the answer to each one.',
             'active-aside' => 9
         ]);
+        Page::insert($this->createParagraph('Here you will find a list of some of the common '
+                . 'questions about command line applications support and the answer to each one.'));
+        $question1 = $this->createQuestionBox('What do you mean by command line application?', ''
+                . 'A command line application is a program that can be executed using terminal '
+                . 'applications such as <code>bash</code>, <code>cmd</code> or <code>cygwin</code>.'
+                . '');
+        Page::insert($question1);
+        $this->setPrevTopicLink('learn/topics/cli/commands-reference', 'Commands Reference');
         $this->displayView();
     }
 }
