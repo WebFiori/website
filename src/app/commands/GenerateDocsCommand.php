9434cbf0af486abf783c92b4f78831884f7390fd
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-11-07 05:14:38 +0300

    Create GenerateDocsCommand.php

---
+++ src/app/commands/GenerateDocsCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace webfiori\framework\cli;
+use webfiori\framework\Logger;
+use webfiori\apiParser\DocGenerator;
+
+class GenerateDocsCommand extends CLICommand{
+    public function __construct() {
+        parent::__construct('--generate-docs', [], '');
+    }
+    public function exec() {
+        Logger::enabled(true);
+        //    $r = new APIReader(ROOT_DIR.'/entity/jsonx/JsonI.php');
+        //    Util::print_r($r->getParsedInfo());
+        $reader = new DocGenerator(array(
+            'path'=>'C:\Server\apache2\htdocs\wfr',
+            'exclude-path'=>array(
+                'C:\Server\apache2\htdocs\wfr\pages',
+                'C:\Server\apache2\htdocs\wfr\apis',
+                'C:\Server\apache2\htdocs\wfr\themes'
+            ),
+            'base-url'=> 'https://webfiori.com/docs',
+            'theme'=>'WebFiori V108',
+            'site-name'=>'WebFiori API Docs',
+            'output-path'=>'C:\\Server\\apache2\\htdocs\\webfiori-docs\\pages\\apis',
+            'route-root-folder'=>'apis',
+            'is-dynamic'=>true
+        ));
+    }
+
+}
