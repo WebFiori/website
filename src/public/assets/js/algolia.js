1ea150505a59da854a0379e27a274b629f43ce48
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2021-04-18 14:56:24 +0300

    Update algolia.js

--- src/public/assets/js/algolia.js
+++ src/public/assets/js/algolia.js
@@ -1,12 +1,39 @@
-const algolia = algoliasearch('IL7ET3Z70M', '22396c5c33e6ed38012120f7523c435c');
+window.algolia = algoliasearch('IL7ET3Z70M', '22396c5c33e6ed38012120f7523c435c');
 window.index = algolia.initIndex('test_wf');
 function search(str) {
-    window.index.search(str).then(({ hits }) => {
-        console.log(hits);
+    var classesIndex = 'test_wf';
+    var methodsIndex = 'test_meth';
+    window.algolia.multipleQueries([
+        {
+            indexName: classesIndex,
+            query: str,
+            params: {
+                hitsPerPage: 3
+            }
+        },
+        {
+            indexName: methodsIndex,
+            query: str,
+            params: {
+                hitsPerPage: 5
+            }
+        }
+    ]).then(({ results  }) => {
+        console.log(results);
         window.app.search_results = [];
-        var len = hits.length > 10 ? 10 : hits.length;
-        for (var x = 0 ; x < len ; x++) {
-            window.app.search_results.push(hits[x]);
+        window.app.methods_search_results = [];
+        
+        for (var x = 0 ; x < results.length ; x++) {
+            var hits = results[x].hits;
+            var len = hits.length > 5 ? 5 : hits.length;
+            
+            for (var y = 0 ; y < len ; y++) {
+                if (results[x].index === classesIndex) {
+                    window.app.search_results.push(hits[y]);
+                } else {
+                    window.app.methods_search_results.push(hits[y]);
+                }
+            }
         }
     });
 }
