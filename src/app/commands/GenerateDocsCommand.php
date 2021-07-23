932f4a36e9870ce6c2e3c8689328141a21cadc52
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2021-07-23 04:34:59 +0300

    Update GenerateDocsCommand.php

--- src/app/commands/GenerateDocsCommand.php
+++ src/app/commands/GenerateDocsCommand.php
@@ -4,29 +4,29 @@ namespace webfiori\framework\cli;
 use webfiori\framework\Logger;
 use webfiori\apiParser\DocGenerator;
 use webfiori\theme\NewFioriAPI;
+
 class GenerateDocsCommand extends CLICommand{
     public function __construct() {
         parent::__construct('--generate-docs', [], '');
     }
     public function exec() {
-        Logger::enabled(true);
         //    $r = new APIReader(ROOT_DIR.'/entity/jsonx/JsonI.php');
         //    Util::print_r($r->getParsedInfo());
         $reader = new DocGenerator(array(
-            'path'=>'C:\Server2\apache2\htdocs\webfiori\src',
+            'path'=>'C:\Server\apache2\htdocs\release',
             'exclude-path'=>array(
-                'C:\Server2\apache2\htdocs\webfiori\src\app\pages',
-                'C:\Server2\apache2\htdocs\webfiori\src\app\apis',
-                'C:\Server2\apache2\htdocs\webfiori\src\themes',
-                'C:\Server2\apache2\htdocs\webfiori\src\app\database',
-                'C:\Server2\apache2\htdocs\webfiori\src\app\middleware',
-                'C:\Server2\apache2\htdocs\webfiori\src\app\storage',
-                
+                'C:\Server\apache2\htdocs\release\app\pages',
+                'C:\Server\apache2\htdocs\release\src\app\apis',
+                'C:\Server\apache2\htdocs\release\src\themes',
+                'C:\Server\apache2\htdocs\release\src\app\database',
+                'C:\Server\apache2\htdocs\release\src\app\middleware',
+                'C:\Server\apache2\htdocs\release\src\app\storage',
+                'C:\Server\apache2\htdocs\release\src\themes',
             ),
-            'base-url'=> 'http://localhost:8989/docs',
+            'base-url'=> 'https://webfiori.com/docs',
             'theme'=> NewFioriAPI::class,
             'site-name'=>'WebFiori API Docs',
-            'output-path'=>'C:\\Server2\\apache2\\htdocs\\webfiori-docs\\src\\app\\pages\\apis',
+            'output-path'=>'C:\\Server\\apache2\\htdocs\\website\\src\\app\\pages\\apis',
             'route-root-folder'=>'apis',
             'is-dynamic'=>true
         ));
